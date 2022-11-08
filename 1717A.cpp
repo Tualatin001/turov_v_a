37b1abba687676c5870f74ed601925f63fd8ddc9
Tualatin001 <117137729+Tualatin001@users.noreply.github.com>
2022-11-08 15:33:12 +0700

    Update 1717A.cpp

--- 1717A.cpp
+++ 1717A.cpp
@@ -2,28 +2,23 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-int men(int a, int b) {
-	if(a<b) return a;
-	else return b;
-}
-int bol(int a,int b) {
-	if(a>b) return a;
-	else return b;
-}
 int main() {
-	long int a=0,b=0,c=0,d=0;
-	long int n=0,t=0;
+	long long int a=0,b=0,c=0,d=0;
+	long long int  n=0,t=0;
 	std::cin>>t;
 	while(t--) {
 		a=0;
 		std::cin>>n;
-
-		for(int j=1;j<=n;j++) {
-		if(j%3==0&&j/3>=1) a++;
-		if(j*3<=n) a++;
-		if(j*2<=n) a++;
-		if(j%2==0&&j/2>=1) a++;
-		a++;
+		if(n>20) {
+		a=(int)n/2+(int)n/3+(int)n/2+(int)n/3+n;	
+		} else {
+			for(int j=1;j<=n;j++) {
+				if(j%3==0) a++;
+				if(j%2==0)a++;
+				if(j*2<=n)a++;
+				if(j*3<=n) a++;
+				a++;
+			}
 		}
 		std::cout<<a<<'\n';
 	}
